ff9a8e4e6c1d0a60588d19c3931269470730d990
muneeb56-ai <muneebshafiq789@gmail.com>
2026-09-18 23:35:37 +0500

    Day 2 PTratice

---
+++ day 1 test.py
@@ -0,0 +1,21 @@
+name = "MUNEEB"
+print(f"my name is {name}")
+age = "28"
+print(f"my age is {age} years old")
+
+a = "Muneeb"
+b = 28
+c = 3.5
+d = True
+
+print (type(a),  type (b), type(c), type(d))
+my_name = "Muneeb"
+my_age = 28
+my_studies = "M.Sc (Hons)"
+my_goal = "earn $500 per day"
+
+print(f"my name is {my_name} and I'm {my_age} years old and my qualification is {my_studies} and my goal is to {my_goal}")
+
+grocery = ['milk', 'eggs','yogurt']
+grocery.append("soap")
+print(grocery)
---
+++ day 2.py
@@ -0,0 +1,53 @@
+# Cities = ["Okara", "Sahiwal", "Pakpattan", "Patoki"]
+# print(Cities)
+# print(Cities[2])
+# print(Cities[-1])
+# print(Cities[1])
+# print(Cities[0])
+# print(Cities[3])
+# print(len(Cities))
+grocery_items = ["soap", "shampoo", "tea", "sugar", "oil"]
+for items in grocery_items:
+    print(items)
+# print(grocery_items[0])
+# print(grocery_items[1])
+# print(grocery_items[2])
+# print(grocery_items[3])
+# print(grocery_items[4])
+# print(len(grocery_items))
+# grocery_items.append("salt")
+# grocery_items[0]="butter"  ##this is used to replace the value using position index
+# print(grocery_items)
+# print(len(grocery_items))
+marks = [25,31,47,79,60,58]
+# print(marks)
+# print(marks[5])
+# print(len(marks))
+# # print(len(marks[3]))
+# print(max(marks))
+# print(min(marks))
+# print(sum(marks))  
+# print(sum(marks)/len(marks))    ##this is used to calculate the average of the marks
+# print(if (marks>= 50)"Pass", else "Fail")  ##this is used to check the pass or fail of the student
+# for i in marks:
+#     if i>=50:
+#         print("Pass")
+#     else:
+#         print("Fail")
+
+# print("Pass" if marks>=50 else"Fail")
+for m in marks:
+    # print("Pass" if m>=50 else"Fail" )
+    print(m,"Pass" if m>=50 else "Fail")
+print("Done")
+
+
+members = ["Ali", "Hamza", "Amir", "Raza", "Fahad"]
+for persons in members:
+    print("Welcome ,", persons)
+
+
+
+    scores = [5 , 52,63,15,18,79,67,91]
+    total = 0
+    for score in scores:
